5b315d276e3ec82b8a15e418192df94af1d24d36
Hans-Nikolai Viessmann <hv15@hw.ac.uk>
2020-10-16 16:23:29 +0200

    Publishing to master

--- en.search.min.fcf413d9fd0dbac271b7f4a566fbb7dbd3af1923d747df89f73aee465fc51215.js
+++ en.search.min.fcf413d9fd0dbac271b7f4a566fbb7dbd3af1923d747df89f73aee465fc51215.js
@@ -2,7 +2,7 @@
 const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();e.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.64f59e973b95dc54cdb5d060cddf401a02311d97dc4aa14bf7c869655f3762b9.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.2d91b076cfc8d0621312d1ed9862febe6ccfe8a993d32fc03155ea5c65a1f9e8.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
